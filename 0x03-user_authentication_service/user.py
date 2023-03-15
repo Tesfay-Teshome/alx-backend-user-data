495471fc03a4aed797ef64a7e8cd3323da322b80
Tesfay-Teshome <tesfix2017@gmail.com>
2023-03-15 09:24:20 +0300

    User Model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """ User Model """
 
-from sqlalchemy import Colmun, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Colmun(Integer, primary_key=True)
-    email = Colmun(String(250), nullable=False)
-    hashed_password = Colmun(String(250), nullable=False)
-    session_id = Colmun(String(250))
-    reset_token = Colmun(String(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
