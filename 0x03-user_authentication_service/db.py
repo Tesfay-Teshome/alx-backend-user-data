950049991ff2579cd5780c8b4312a16d07aa9822
Tesfay-Teshome <tesfix2017@gmail.com>
2023-03-16 16:39:13 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Database for ORM """
+""" Database for Object Reational Mapping """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
